6c2f6bfb587911a24213a715b5ba86f7b3633cc6
meck <Johan@meck.se>
2025-05-14 11:07:31 +0200

    NixOs module: add option to use UWSM
    This is mostly adapted from the equivelent nixpgks hyprland option

--- flake.nix
+++ flake.nix
@@ -418,6 +418,16 @@
           type = nixpkgs.lib.types.package;
           default = (make-package-set pkgs).niri-stable;
         };
+        withUWSM =
+          nixpkgs.lib.mkEnableOption null
+          // {
+            description = ''
+              Launch niri with the UWSM (Universal Wayland Session Manager) session manager.
+              ::: {.note}
+              Requires extra configuration of greetd or from a display manager like SDDM
+              :::
+            '';
+          };
       };
 
       options.niri-flake.cache.enable = nixpkgs.lib.mkOption {
@@ -441,7 +451,7 @@
             icons.enable = nixpkgs.lib.mkDefault true;
           };
         }
-        (nixpkgs.lib.mkIf cfg.enable {
+        (nixpkgs.lib.mkIf (cfg.enable && !cfg.withUWSM) {
           services =
             if nixpkgs.lib.strings.versionAtLeast config.system.nixos.release "24.05"
             then {
@@ -450,6 +460,26 @@
             else {
               xserver.displayManager.sessionPackages = [cfg.package];
             };
+        })
+        (nixpkgs.lib.mkIf (cfg.enable && cfg.withUWSM) {
+          programs.uwsm = {
+            enable = true;
+            waylandCompositors = {
+              niri = {
+                prettyName = "Niri";
+                comment = "Niri compositor managed by UWSM";
+                # Only takes a path, and `niri-session` uses
+                # the niri default systemd units
+                binPath = nixpkgs.lib.getExe (
+                  pkgs.writeShellScriptBin "niriSession" ''
+                    exec /run/current-system/sw/bin/niri --session
+                  ''
+                );
+              };
+            };
+          };
+        })
+        (nixpkgs.lib.mkIf cfg.enable {
           hardware =
             if nixpkgs.lib.strings.versionAtLeast config.system.nixos.release "24.11"
             then {
