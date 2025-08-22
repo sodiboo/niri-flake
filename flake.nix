3b35a705760411ddbf91801a96de26dee40366b0
sodiboo <git@sodi.boo>
2025-08-22 02:48:34 +0200

    xwayland-satellite: 0.6 -> 0.7

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     niri-stable.url = "github:YaLTeR/niri/v25.05.1";
     niri-unstable.url = "github:YaLTeR/niri";
 
-    xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.6";
+    xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.7";
     xwayland-satellite-unstable.url = "github:Supreeeme/xwayland-satellite";
 
     # they do all have flakes, but we specifically want just the Rust sources and no flakes.
