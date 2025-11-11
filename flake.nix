fba5dd5f41880a9b4df261a2629dea9580a02f79
zimward <zimward@zimward.moe>
2025-11-11 17:37:28 +0100

    use soteria instead of kde-polkit-agent as its a lot faster

--- flake.nix
+++ flake.nix
@@ -495,20 +495,8 @@
               };
 
               security.polkit.enable = true;
+              security.soteria.enable = true;
               services.gnome.gnome-keyring.enable = true;
-              systemd.user.services.niri-flake-polkit = {
-                description = "PolicyKit Authentication Agent provided by niri-flake";
-                wantedBy = [ "niri.service" ];
-                after = [ "graphical-session.target" ];
-                partOf = [ "graphical-session.target" ];
-                serviceConfig = {
-                  Type = "simple";
-                  ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-                  Restart = "on-failure";
-                  RestartSec = 1;
-                  TimeoutStopSec = 10;
-                };
-              };
 
               security.pam.services.swaylock = { };
               programs.dconf.enable = nixpkgs.lib.mkDefault true;
