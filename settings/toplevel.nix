3c29d4a13c9eaf2116bba985996dfdbf3fbbae7f
sodiboo <git@sodi.boo>
2025-12-06 03:17:31 +0100

    factor out workspaces

--- settings/toplevel.nix
+++ settings/toplevel.nix
@@ -79,48 +79,14 @@ in
         };
       }
       {
-        workspaces =
-          attrs-record (key: {
-            name = optional types.str key // {
-              defaultText = "the key of the workspace";
-              description = ''
-                The name of the workspace. You set this manually if you want the keys to be ordered in a specific way.
-              '';
-            };
-            open-on-output = nullable types.str // {
-              description = ''
-                The name of the output the workspace should be assigned to.
-              '';
-            };
-          })
-          // {
-            description = ''
-              Declare named workspaces.
-
-              Named workspaces are similar to regular, dynamic workspaces, except they can be
-              referred to by name, and they are persistent, they do not close when there are
-              no more windows left on them.
-
-              Usage is like so:
-
-              ${fmt.nix-code-block ''
-                {
-                  ${options.workspaces}."name" = {};
-                  ${options.workspaces}."01-another-one" = {
-                    open-on-output = "DP-1";
-                    name = "another-one";
-                  };
-                }
-              ''}
-
-              Unless a ${fmt.code "name"} is declared, the workspace will use the attribute key as the name.
-
-              Workspaces will be created in a specific order: sorted by key. If you do not care
-              about the order of named workspaces, you can skip using the ${fmt.code "name"} attribute, and
-              use the key instead. If you do care about it, you can use the key to order them,
-              and a ${fmt.code "name"} attribute to have a friendlier name.
-            '';
-          };
+        workspaces = import ./workspaces.nix {
+          inherit
+            lib
+            kdl
+            niri-flake-internal
+            toplevel-options
+            ;
+        };
       }
 
       {
